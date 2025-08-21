12d4905a945fa6f1d66b8491c56b4120a743e13f
mvargasmoran <mvargasmoran@gmail.com>
2025-08-21 00:18:45 -0400

    Adding some project TODO

---
+++ lua/collector/project.lua
@@ -0,0 +1,30 @@
+-- FEATURES
+
+-- TODO: Sorting:
+-- -- TODO: by boring alphabetical, it does help with searching with your eyes
+-- -- TODO: by CMC
+-- -- TODO: by color pips in WUBRG order
+-- -- TODO: by super type
+
+-- TODO: Grouping:
+-- -- TODO: by Scryfall tags
+-- -- -- TODO: support custom user tags
+-- -- -- TODO: add that weird column sorting a user can do: 1=combo-package;2=kill;3=exile;4=bounce;5=ramp;6=draw
+-- -- TODO: by Card super Type
+-- -- TODO: by Card color
+-- -- TODO: by Card CMC
+
+-- TODO: Generate Curve
+-- -- TODO: Generate Countings (STATS)
+-- -- TODO: Supertype
+-- -- TODO: Tags
+
+
+-- DEBT
+
+-- TODO: Get a good hashing thing
+-- DONE: trim whitespace at the end
+-- DONE: clean lines from weird unicode characters for any math purposes
+-- TODO: put back the "nice" but weird unicode characters, is this one: —
+-- TODO: Add empty line padding if needed, to match some "similar height" for all cards
+-- TODO: Move to a database
